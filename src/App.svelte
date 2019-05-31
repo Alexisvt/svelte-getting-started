b36e599e75d360b7e05352f3c41a1b5ca690e121
Alexis Villegas Torres <alexisvt@gmail.com>
2019-05-30 23:14:20 -0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  let name = "Alexis";
+  let age = 33;
+
+  $: uppercaseName = name.toUpperCase();
+
+  function incrementAge() {
+    age += 1;
+  }
+
+  function changeName() {
+    name = "David";
+  }
+
+  function nameInput({ target }) {
+    name = target.value;
+  }
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+<input type="text" value={name} on:input={nameInput} />
